738ec668ffae029cda04891916f0b70cd2a01547
Ekin Aslan <ekinaslan.js@gmail.com>
2022-07-11 16:29:59 +0300

    Menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,27 @@
-var navigation = document.querySelector("nav");
-var logo = document.querySelector("#logo");
+/* Variables //////////////////////////////////////// */
 
-window.addEventListener("scroll", function () {
+var navigation = document.querySelector("nav");
+var logo = navigation.querySelector("#logo");
+var menu = navigation.querySelector("#menu");
+var categories = navigation.querySelector("nav ul");
+var categoryButtons = navigation.querySelectorAll("nav ul > *");
+
+/* Event //////////////////////////////////////// */
+
+window.addEventListener("scroll", scrollCheck);
+
+menu.addEventListener("click", toggleMenu);
+categoryButtons.forEach(function (item) {
+  item.addEventListener("click", toggleMenu);
+});
+
+/* Function //////////////////////////////////////// */
+
+function toggleMenu() {
+  categories.classList.toggle("hidden");
+}
+
+function scrollCheck() {
   if (window.scrollY == 0) {
     navigation.classList.remove("smallNav");
     logo.classList.remove("smallLogo");
@@ -9,4 +29,4 @@ window.addEventListener("scroll", function () {
     navigation.classList.add("smallNav");
     logo.classList.add("smallLogo");
   }
-});
+}
