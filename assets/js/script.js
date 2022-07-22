b9551cc6f91a23607d0530feb3031c92e7c854a3
Ekin Aslan <ekinaslan.js@gmail.com>
2022-07-22 17:26:28 +0300

    Game Icon Content

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,16 @@
 var navigation = document.querySelector("nav");
 var logo = navigation.querySelector("#logo");
 var menu = navigation.querySelector("#menu");
-var categories = navigation.querySelector("nav ul");
-var categoryButtons = navigation.querySelectorAll("nav ul > *");
+var categories = navigation.querySelector("ul");
+var categoryButtons = navigation.querySelectorAll("ul > *");
 
-/* Event //////////////////////////////////////// */
+var slides = document.querySelectorAll("section .dual-content .slider > *");
+
+/* Initials //////////////////////////////////////// */
+
+setInterval(slide, 5000);
+
+/* Events //////////////////////////////////////// */
 
 window.addEventListener("scroll", scrollCheck);
 
@@ -15,7 +21,7 @@ categoryButtons.forEach(function (item) {
   item.addEventListener("click", toggleMenu);
 });
 
-/* Function //////////////////////////////////////// */
+/* Functions //////////////////////////////////////// */
 
 function toggleMenu() {
   categories.classList.toggle("hidden");
@@ -28,5 +34,31 @@ function scrollCheck() {
   } else {
     navigation.classList.add("smallNav");
     logo.classList.add("smallLogo");
+  }
+}
+
+function slide() {
+  for (let i = 0; i < slides.length; i++) {
+    let item = slides[i];
+
+    if (!item.classList.contains("faded")) {
+      let nextItem = slides[(i + 1) % slides.length];
+      let nextOtherItem = slides[(i + 2) % slides.length];
+
+      item.classList.add("faded");
+      item.classList.add("left");
+
+      setTimeout(function () {
+        nextItem.classList.remove("faded");
+        nextItem.classList.remove("right");
+      }, 300);
+
+      setTimeout(function () {
+        item.classList.remove("left");
+        nextOtherItem.classList.add("right");
+      }, 1000);
+
+      break;
+    }
   }
 }
